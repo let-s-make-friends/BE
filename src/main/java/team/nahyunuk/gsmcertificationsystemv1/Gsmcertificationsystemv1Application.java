2761b1d0ce03fd56fe0c9d83d581dac7fa9bab7d
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-02-05 13:22:09 +0900

    init

---
+++ src/main/java/team/nahyunuk/gsmcertificationsystemv1/Gsmcertificationsystemv1Application.java
@@ -0,0 +1,13 @@
+package team.nahyunuk.gsmcertificationsystemv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gsmcertificationsystemv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gsmcertificationsystemv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/team/nahyunuk/gsmcertificationsystemv1/Gsmcertificationsystemv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package team.nahyunuk.gsmcertificationsystemv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gsmcertificationsystemv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
