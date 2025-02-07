f4e4d173eef8ec50ae7d770aa9716f9ee113e797
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-02-07 17:38:38 +0900

    feat: Add CommonApiResponse

--- src/main/java/team/nahyunuk/gsmcertificationsystemv1/global/common/response/CommonApiResponse.java
+++ src/main/java/team/nahyunuk/gsmcertificationsystemv1/global/common/response/CommonApiResponse.java
@@ -1,4 +1,17 @@
 package team.nahyunuk.gsmcertificationsystemv1.global.common.response;
 
-public class CommonApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonApiResponse<T> {
+    private HttpStatus status;
+    private int code;
+    private String message;
+    private T data;
+
 }
