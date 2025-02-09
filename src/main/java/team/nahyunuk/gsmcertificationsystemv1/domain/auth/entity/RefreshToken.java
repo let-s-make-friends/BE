84196feacf1d660573ace217f36426825d78d6f4
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-02-09 15:06:51 +0900

    feat: RefreshToken 엔티티 추가

---
+++ src/main/java/team/nahyunuk/gsmcertificationsystemv1/domain/auth/entity/RefreshToken.java
@@ -0,0 +1,4 @@
+package team.nahyunuk.gsmcertificationsystemv1.domain.auth.entity;
+
+public class RefreshToken {
+}
---
+++ src/main/java/team/nahyunuk/gsmcertificationsystemv1/domain/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,4 @@
+package team.nahyunuk.gsmcertificationsystemv1.domain.auth.repository;
+
+public class RefreshTokenRepository {
+}
