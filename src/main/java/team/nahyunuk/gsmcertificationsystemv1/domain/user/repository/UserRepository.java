f9454e69dcd1b766d91b3b433e3845d25190c918
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-02-09 21:44:12 +0900

    feat: Add loadUserByUsername method in CustomUserDetailsService

--- src/main/java/team/nahyunuk/gsmcertificationsystemv1/domain/user/repository/UserRepository.java
+++ src/main/java/team/nahyunuk/gsmcertificationsystemv1/domain/user/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package team.nahyunuk.gsmcertificationsystemv1.domain.user.repository;
 
-public class UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import team.nahyunuk.gsmcertificationsystemv1.domain.user.entity.User;
+
+import java.util.UUID;
+
+public interface UserRepository extends CrudRepository<User, UUID> {
 }
--- src/main/java/team/nahyunuk/gsmcertificationsystemv1/global/security/auth/CustomUserDetailsService.java
+++ src/main/java/team/nahyunuk/gsmcertificationsystemv1/global/security/auth/CustomUserDetailsService.java
@@ -1,5 +1,26 @@
 package team.nahyunuk.gsmcertificationsystemv1.global.security.auth;
 
-public class CustomUserDetailsService {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import team.nahyunuk.gsmcertificationsystemv1.domain.user.repository.UserRepository;
+import team.nahyunuk.gsmcertificationsystemv1.global.exception.CustomException;
+import team.nahyunuk.gsmcertificationsystemv1.global.exception.error.ErrorCode;
 
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        return new CustomUserDetails(userRepository.findById(UUID.fromString(userId))
+                .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND)));
+    }
 }
