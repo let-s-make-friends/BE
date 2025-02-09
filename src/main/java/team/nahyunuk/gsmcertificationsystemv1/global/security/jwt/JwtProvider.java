93b1cd1c90d6e2ce1ca8575c79d30e9b7d59399c
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-02-09 17:58:23 +0900

    feat: Add generateAccessToken in JwtProvider

--- src/main/java/team/nahyunuk/gsmcertificationsystemv1/global/security/jwt/JwtProvider.java
+++ src/main/java/team/nahyunuk/gsmcertificationsystemv1/global/security/jwt/JwtProvider.java
@@ -1,8 +1,12 @@
 package team.nahyunuk.gsmcertificationsystemv1.global.security.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.exception.DataException;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Value;
 import io.jsonwebtoken.io.Decoders;
@@ -11,6 +15,7 @@ import team.nahyunuk.gsmcertificationsystemv1.global.security.jwt.dto.TokenDto;
 
 import java.security.Key;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.UUID;
 
 @Component
@@ -39,5 +44,17 @@ public class JwtProvider {
                 .accessTokenExp(LocalDateTime.now().plusSeconds(ACCESS_TOKEN_TIME))
                 .refreshTokenExp(LocalDateTime.now().plusSeconds(REFRESH_TOKEN_TIME))
                 .build();
+    }
+
+    private String generateAccessToken(UUID userId, Authority authority) {
+        Date expiration = new Date(System.currentTimeMillis() + ACCESS_TOKEN_TIME * 1000);
+
+        return Jwts.builder()
+                .setSubject(userId.toString())
+                .claim(AUTHORITIES_KEY, authority)
+                .setIssuedAt(new Date())
+                .setExpiration(expiration)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
     }
 }
