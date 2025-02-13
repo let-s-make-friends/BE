020a6d48d80ebaa1676355cba677115c3cada768
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-02-13 23:22:55 +0900

    feat: Add exception

--- src/main/java/team/nahyunuk/gsmcertificationsystem/v1/global/exception/error/ErrorCode.java
+++ src/main/java/team/nahyunuk/gsmcertificationsystem/v1/global/exception/error/ErrorCode.java
@@ -15,7 +15,7 @@ public enum ErrorCode {
     UNAUTHORIZED(401, "인증되지 않은 사용자입니다."),
     TOKEN_MALFORMED(400, "형식이 잘못된 토큰입니다."),
     MISSING_TOKEN(400, "토큰이 누락되었습니다."),
-    FAILED_JWT_AUTH(401, "jwt 인증에 실패했습니다"),
+    FAILED_JWT_AUTH(401, "jwt 인증에 실패했습니다."),
     MISSING_REFRESH_TOKEN(400, "헤더에 리프레시 토큰을 추가해주세요."),
 
     // user
@@ -24,10 +24,13 @@ public enum ErrorCode {
     EXPIRED_OR_INVALID_CODE(400, "만료되었거나 유효하지 않은 인증 코드입니다."),
     INVALID_CODE(400, "잘못된 인증 코드입니다."),
     EMAIL_NOT_VERIFIED(401, "이메일 인증이 완료되지 않았습니다."),
-    INVALID_PASSWORD(401, "비밀번호가 일치하지 않습니다"),
+    INVALID_PASSWORD(401, "비밀번호가 일치하지 않습니다."),
+    EMAIL_NOT_PENDING(400, "이메일 인증 대기 상태가 아닙니다."),
+    INVALID_EMAIL_FORMAT(400, "잘못된 이메일 형식입니다."),
 
-    // server
+    // server,
     INTERNAL_SERVER_ERROR(500, "서버 오류"),
+    EMAIL_SEND_FAILID(500, "이메일 발송에 실패했습니다."),
 
     // Optimistic Locking
     OPTIMISTIC_LOCK_FAILURE(409, "낙관적 잠금 예외가 발생했습니다. 다시 시도해주세요.");
