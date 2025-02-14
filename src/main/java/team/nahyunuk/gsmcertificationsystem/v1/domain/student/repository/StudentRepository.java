2e3c1b949736e0fa173bec2e8cdc9a5c857c3df0
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-02-14 15:20:18 +0900

    feat: Add StudentRepository

---
+++ src/main/java/team/nahyunuk/gsmcertificationsystem/v1/domain/student/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package team.nahyunuk.gsmcertificationsystem.v1.domain.student.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import team.nahyunuk.gsmcertificationsystem.v1.domain.student.entity.Student;
+
+public interface StudentRepository extends CrudRepository<Student, String> {
+}
