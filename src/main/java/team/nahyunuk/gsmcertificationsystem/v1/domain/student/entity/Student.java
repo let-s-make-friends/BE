d77addacc0ca74ab9c86d1138819f3fe8aa1e054
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-02-15 21:30:27 +0900

    feat: Add format_sql is true in application.yml

--- src/main/java/team/nahyunuk/gsmcertificationsystem/v1/domain/student/entity/Student.java
+++ src/main/java/team/nahyunuk/gsmcertificationsystem/v1/domain/student/entity/Student.java
@@ -1,4 +1,35 @@
 package team.nahyunuk.gsmcertificationsystem.v1.domain.student.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "student")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+
+    @Id
+    @Column(unique = true, nullable = false, updatable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String studentName;
+
+    @Column(nullable = false)
+    private int grade;
+    @Column(nullable = false)
+    private int classNumber;
+    @Column(nullable = false)
+    private int studentNumber;
+
+
 }
