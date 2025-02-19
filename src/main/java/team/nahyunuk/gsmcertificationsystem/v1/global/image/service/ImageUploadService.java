b7effc9d09efa2013ff868537740ba02671c9f4f
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-02-19 17:36:03 +0900

    refactor: Change responseDto in ImageUploadServiceImpl

--- src/main/java/team/nahyunuk/gsmcertificationsystem/v1/global/image/service/ImageUploadService.java
+++ src/main/java/team/nahyunuk/gsmcertificationsystem/v1/global/image/service/ImageUploadService.java
@@ -1,4 +1,8 @@
 package team.nahyunuk.gsmcertificationsystem.v1.global.image.service;
 
-public interface ImageService {
+import org.springframework.web.multipart.MultipartFile;
+import team.nahyunuk.gsmcertificationsystem.v1.global.response.CommonApiResponse;
+
+public interface ImageUploadService {
+    CommonApiResponse execute(MultipartFile image);
 }
--- src/main/java/team/nahyunuk/gsmcertificationsystem/v1/global/image/service/impl/ImageUploadServiceImpl.java
+++ src/main/java/team/nahyunuk/gsmcertificationsystem/v1/global/image/service/impl/ImageUploadServiceImpl.java
@@ -1,4 +1,21 @@
 package team.nahyunuk.gsmcertificationsystem.v1.global.image.service.impl;
 
-public class ImageUploadServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import team.nahyunuk.gsmcertificationsystem.v1.global.image.service.ImageUploadService;
+import team.nahyunuk.gsmcertificationsystem.v1.global.response.CommonApiResponse;
+import team.nahyunuk.gsmcertificationsystem.v1.global.thirdParty.aws.util.S3Util;
+
+@Service
+@RequiredArgsConstructor
+public class ImageUploadServiceImpl implements ImageUploadService {
+
+    private final S3Util s3Util;
+
+    @Override
+    public CommonApiResponse execute(MultipartFile image) {
+        String imageUrl = s3Util.imageUpload(image);
+        return CommonApiResponse.successWithData("이미지가 저장되었습니다", imageUrl);
+    }
 }
